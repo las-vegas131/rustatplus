35018050bf81ad9374f532acd26cdf78b56c2fdd
las-vegas131 <las-vegas31@yandex.ru>
2026-05-25 17:38:43 +0300

    Update stream.py

--- stream.py
+++ stream.py
@@ -30,7 +30,7 @@ from calculations import (
 )
 from visualization import (
     create_player_radar_figure, create_compare_figure, create_position_radar,
-    export_matches_advanced, export_match_standard_with_charts
+    export_matches_advanced, export_match_standard_position_tables
 )
 
 st.set_page_config(page_title="InStat Analyst", layout="wide")
